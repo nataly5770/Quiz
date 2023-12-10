b9eb1bad111741107ff790eca657456035d3db37
Natalja <natkja@gmail.com>
2023-12-11 00:16:45 +0200

    Here I have added more countries and its capitals

--- src/QuizData.js
+++ src/QuizData.js
@@ -1,21 +1,21 @@
 export const QuizData = [
     {
         id: 0,
-        question: `What is the capital of Nigeria?`,
-        options: [`New Delhi`, `Abuja`, `Owerri`, `Enugu`],
-        answer: `Abuja`
-    },
-    {
-        id: 0,
-        question: `What is the capital of India?`,
-        options: [`New Delhi`, `Abuja`, `Mumbai`, `New DelhiAba`],
-        answer: `New Delhi`
+        question: `What is the capital of Afghanistan?`,
+        options: [`New Delhi`, `Kabul`, `Owerri`, `Enugu`],
+        answer: `Kabul`
     },
     {
         id: 0,
         question: `What is the capital of Australia?`,
-        options: [`Melbourne`, `Akokwa`, `Owerri`, `Sydney`],
-        answer: `Sydney`
+        options: [`New Delhi`, `Abuja`, `Mumbai`, `Canberra`],
+        answer: `Canberra`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Bahamas?`,
+        options: [`Melbourne`, `Akokwa`, `Nassau`, `Sydney`],
+        answer: `Nassau`
     },
     {
         id: 0,
@@ -29,4 +29,145 @@ export const QuizData = [
         options: [`Syria`, `Damascus`, `Anambra`, `Enugu`],
         answer: `Damascus`
     },
+
+    {
+        id: 0,
+        question: `What is the capital of Georgia?`,
+        options: [`Tbilisi`, `Libreville`, `Anambra`, `Bissau`],
+        answer: `Tbilisi`
+    },
+    {
+        id: 0,
+        question: `What is the capital of France?`,
+        options: [`Moscos`, `Paris`, `Anambra`, `Tallinn`],
+        answer: `Paris`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Iceland?`,
+        options: [`Jakarta`, `Reykjavik`, `Taipei`, `Tunis`],
+        answer: `Reykjavik`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Ireland?`,
+        options: [`Lomé`, `Bern`, `Dublin`, `Bishkek`],
+        answer: `Dublin`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Kazakhstan?`,
+        options: [`Astana`, `Damascus`, `Montevideo`, `London`],
+        answer: `Astana`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Luxembourg?`,
+        options: [`Lusaka`, `Nairobi`, `Luxembourg (city)`, `Bishkek`],
+        answer: `Damascus`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Latvia?`,
+        options: [`Conakry`, `Beirut`, `Riga`, `Accra`],
+        answer: `Riga`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Finland?`,
+        options: [`Helsinki`, `Juba`, `Paris`, `Bissau`],
+        answer: `Helsinki`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Denmark?`,
+        options: [`Banjul`, `Zagreb`, `Cairo`, `Copenhagen`],
+        answer: `Copenhagen`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Germany?`,
+        options: [`Ljubljana`, `Berlin`, `Bratislava`, `Athens`],
+        answer: `Berlin`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Romania?`,
+        options: [`Lisbon`, `Damascus`, `Bucharest`, `Lima`],
+        answer: `Bucharest`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Cuba?`,
+        options: [`Bangui`, `Havana`, `Anambra`, `Doha`],
+        answer: `Havana`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Thailand?`,
+        options: [`Taipei`, `Bangkok`, `Dushanbe`, `Dodoma`],
+        answer: `Bangkok`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Tunisia?`,
+        options: [`Tunis`, `Damascus`, `Ankara`, `Enugu`],
+        answer: `Tunis`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Romania?`,
+        options: [`Bucharest`, `Sibiu`, `Arad`, `Ovidiu`],
+        answer: `Bucharest`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Qatar?`,
+        options: [`Abu Samra`, `Doha`, `Anambra`, `Al Hitmi`],
+        answer: `Doha`
+    },
+    {
+        id: 0,
+        question: `What is the capital of China?`,
+        options: [`Shanghai`, `Beijing`, `Tianjin`, `Nanjing`],
+        answer: `Beijing`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Serbia?`,
+        options: [`Kragujevac`, `Vranje`, `Anambra`, `Belgrade`],
+        answer: `Belgrade`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Czechia?`,
+        options: [`Prague`, `Liberec`, `Olomouc`, `Jihlava`],
+        answer: `Prague`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Cyprus?`,
+        options: [`Limassol`, `Paphos`, `Nicosia`, `Pyla`],
+        answer: `Nicosia`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Egypt?`,
+        options: [`Alexandria`, `Cairo`, `Luxor`, `Asyut`],
+        answer: `Cairo`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Greece?`,
+        options: [`Athens`, `Rhodes`, `Larissa`, `Piraeus`],
+        answer: `Athens`
+    },
+    {
+        id: 0,
+        question: `What is the capital of Italy?`,
+        options: [`Rome`, `Jerusalem`, `Jakarta`, `Sicily`],
+        answer: `Rome`
+    },
+
+
 ]
